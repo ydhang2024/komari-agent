4cd8626d35375a2cee6de204771fb598beb8372c
Akizon77 <i@akz.moe>
2025-05-25 00:54:08 +0800

    fix:  Windows 11 版本识别

--- monitoring/unit/os_windows.go
+++ monitoring/unit/os_windows.go
@@ -4,6 +4,9 @@
 package monitoring
 
 import (
+	"strconv"
+	"strings"
+
 	"golang.org/x/sys/windows/registry"
 )
 
@@ -17,6 +20,26 @@ func OSName() string {
 	productName, _, err := key.GetStringValue("ProductName")
 	if err != nil {
 		return "Microsoft Windows"
+	}
+
+	// Windows 11
+	majorVersion, _, err := key.GetIntegerValue("CurrentMajorVersionNumber")
+	if err == nil && majorVersion >= 10 {
+		buildNumberStr, _, err := key.GetStringValue("CurrentBuild")
+		if err == nil {
+			buildNumber, err := strconv.Atoi(buildNumberStr)
+			if err == nil && buildNumber >= 22000 { // Windows 11 starts at build 22000
+				// Windows 11 Windows 10 Pro for Workstations
+				edition := strings.Replace(productName, "Windows 10 ", "", 1)
+				return "Windows 11 " + edition
+			}
+		}
+		// DisplayVersion
+		displayVersion, _, err := key.GetStringValue("DisplayVersion")
+		if err == nil && displayVersion >= "21H2" {
+			edition := strings.Replace(productName, "Windows 10 ", "", 1)
+			return "Windows 11 " + edition
+		}
 	}
 
 	return productName
